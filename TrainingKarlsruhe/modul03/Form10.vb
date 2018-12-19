3a18276030394bdc804adbd69088c3ee881a2b37
Hannes Preishuber <hannesp@ppedv.de>
2018-12-19 15:59:50 +0100

    select Case Keyboard steuerung

--- TrainingKarlsruhe/modul03/Form10.vb
+++ TrainingKarlsruhe/modul03/Form10.vb
@@ -1,17 +1,17 @@
 ﻿Public Class Form10
 
     Private Sub Form10_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
-
+        Debug.WriteLine(e.KeyCode.ToString())
 
         Select Case e.KeyCode.ToString()
-            Case "L"
+            Case "Left"
                 Label1.Left -= 1
-            Case "R"
+            Case "Right"
                 Label1.Left += 1
 
-            Case "U"
+            Case "Down"
                 Label1.Top += 1
-            Case "O"
+            Case "Up"
                 Label1.Top -= 1
             Case Else
                 MessageBox.Show("nur L R U O Taste")

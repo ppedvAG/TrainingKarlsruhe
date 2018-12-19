db74b52e1d8aa30b87a1c15d4d7416cc2c18fe8a
Hannes Preishuber <hannesp@ppedv.de>
2018-12-19 16:45:53 +0100

    Array und for Each

---
+++ TrainingKarlsruhe/modul03/Form12.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form12
+
+    Dim a(6) As Date
+    Dim clickcount As Integer = 0
+    Private Sub Form12_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        a(clickcount) = Date.Now
+        clickcount += 1
+        If clickcount >= a.Length Then
+            ReDim Preserve a(a.Length + 12)
+        End If
+        ListBox1.DataSource = a
+
+
+        'Dim aa As New ArrayList
+
+
+        'For index = 1 To a.Length
+
+        'Next
+        For Each franz As Date In a
+            Debug.WriteLine(franz)
+        Next
+
+
+    End Sub
+End Class

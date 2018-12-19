2c5790ebe2d0a9e756c897cc40be8ee309bc5e09
Hannes Preishuber <hannesp@ppedv.de>
2018-12-19 09:56:57 +0100

    Init2

--- TrainingKarlsruhe/Form1.vb
+++ TrainingKarlsruhe/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("Hallo ")
+        'MessageBox.Show("Hallo")
+        'TODO: hab ich noch was vor
     End Sub
 End Class

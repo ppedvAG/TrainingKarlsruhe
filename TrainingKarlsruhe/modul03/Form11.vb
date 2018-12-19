1cc078625f980e4d31588a149d219e1f56427197
Hannes Preishuber <hannesp@ppedv.de>
2018-12-19 16:25:23 +0100

    While Loop

---
+++ TrainingKarlsruhe/modul03/Form11.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form11
+    Private Sub Form11_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Int16 = 1
+        Dim ii As Int16 = 1
+        While i > 3000
+            i += ii
+        End While
+
+
+
+
+        Do
+            i += ii
+        Loop Until i > 3000
+
+        While True
+            If i > 3000 Then
+                Exit While
+            End If
+
+        End While
+
+
+        'For i = 1 To 10
+
+        'Next
+    End Sub
+End Class

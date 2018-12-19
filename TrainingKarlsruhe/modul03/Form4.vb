2eba3c576f098c813881001b74977e0da33d16a7
Hannes Preishuber <hannesp@ppedv.de>
2018-12-19 11:46:53 +0100

    Date, Formatieren, Rechnen

---
+++ TrainingKarlsruhe/modul03/Form4.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form4
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim d As New Date
+        d = Date.Today
+
+        'Dim dd= Date.Today
+
+        'Label1.Text = d.AddDays(-200).ToLongDateString
+
+        Label1.Text = d.ToString("yyyy-MMM.-ddd")
+
+
+    End Sub
+End Class

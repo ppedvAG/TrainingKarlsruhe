98b87ea02f2e5445699f61da8dd068acc467eae3
Hannes Preishuber <hannesp@ppedv.de>
2018-12-19 10:28:23 +0100

    datentypen

--- TrainingKarlsruhe/Form1.vb
+++ TrainingKarlsruhe/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'MessageBox.Show("Hallo")
+        '  MessageBox.Show("Hallo")
         'TODO: hab ich noch was vor
+        Dim i = 10
+        Dim a = 1 / 0
+        MessageBox.Show(Nothing, "text", "Titel", MessageBoxButtons.OKCancel)
     End Sub
 End Class
---
+++ TrainingKarlsruhe/modul03/Form2.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Integer
+        Dim b As Boolean
+        Dim o As Object
+
+        i = 1
+        i = "a"
+        i = 1.2
+
+        o = i
+
+
+
+    End Sub
+End Class

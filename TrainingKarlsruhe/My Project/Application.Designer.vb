4f8f12ccaffbfc9b083d999959216c92966bafe0
Hannes Preishuber <hannesp@ppedv.de>
2018-12-19 14:28:16 +0100

    If For Quersumme

--- TrainingKarlsruhe/My Project/Application.Designer.vb
+++ TrainingKarlsruhe/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TrainingKarlsruhe.Form7
+            Me.MainForm = Global.TrainingKarlsruhe.Form8
         End Sub
     End Class
 End Namespace
---
+++ TrainingKarlsruhe/modul4/Form8.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form8
+    Private Sub Form8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Quersumme
+        '  Dim counter = 4
+        Dim zahl = "1234567890"
+        'If counter < zahl.Length Then
+        '    counter += 1   '=+ ACHTUNG
+        '    ' counter = counter + 1
+        'ElseIf counter = 4 And zahl <> "1234" Then
+        '    'mach was anderes
+        '    Dim xhannes = 0
+        'Else
+        '    'ende
+        'End If
+        'If zahl.Substring(0, 4) = "1234" Then
+        '    Dim xhannes = 0
+        'End If
+        Dim quer As Integer = 0
+        For index = 1 To zahl.Length
+            quer += CInt(zahl.Substring(index - 1, 1))
+            If quer > 99 Then
+                Exit For
+            End If
+        Next
+        'For index = 0 To zahl.Length - 1
+        '    zahl.Substring(index, 1)
+
+
+    End Sub
+End Class

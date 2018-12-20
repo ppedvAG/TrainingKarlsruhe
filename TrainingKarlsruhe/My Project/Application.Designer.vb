63032fc2337d6103d2dd9902e9bc2688f1ebb2fb
Hannes Preishuber <hannesp@ppedv.de>
2018-12-20 10:05:25 +0100

    Function, Select Case

--- TrainingKarlsruhe/My Project/Application.Designer.vb
+++ TrainingKarlsruhe/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TrainingKarlsruhe.Form13
+            Me.MainForm = Global.TrainingKarlsruhe.Form15
         End Sub
     End Class
 End Namespace
---
+++ TrainingKarlsruhe/modul05/Form15.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form15
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = Anrede(TextBox1.Text)
+    End Sub
+    Function Anrede(Buchstabe As String) As String
+        Select Case Buchstabe.ToUpper  'kleinbuchstabe -> Großbuchstabe
+            Case "M"
+                Return "Herr"
+            Case "W"
+                Return "Frau"
+
+            Case Else
+                Return "Hallo"
+        End Select
+
+
+    End Function
+    Function Anrede(Buchstabe As Integer) As String
+
+    End Function
+End Class

9cafc5db7a1216cd7a5ec46374fc8cfd88e84dee
Hannes Preishuber <hannesp@ppedv.de>
2018-12-20 12:26:45 +0100

    Nuget System.ValueTuple Tuple

---
+++ TrainingKarlsruhe/modul06/Form19.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form19
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Location = New System.Drawing.Point(12, 32)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(800, 31)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Hallo Welt"
+        '
+        'Form19
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form19"
+        Me.Text = "Form19"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ TrainingKarlsruhe/modul06/Form19.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form19
+    Private Sub Form19_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TextBox1.BackColor = Color.AliceBlue
+        'Dim farbe = TextBox1.BackColor
+        'TextBox1.Text = "Wert"
+
+        Dim t = (1, "Hannes", "Ach", #08/09/1968#)
+
+        Dim satz = $"Hallo {t.Item2}, du bist hier nummer {t.Item1} und wohnst in {t.Item3} und 
+bist geboren am {t.Item4:d}"
+
+
+        Dim tt = (id:=1, Name:="Hannes", ort:="Ach", gebdatum:=#08/09/1968#)
+
+        satz = $"Hallo {tt.id}, du bist hier nummer {tt.Name}und wohnst in {tt.ort} und 
+bist geboren am {tt.gebdatum:d}"
+        'antik
+        'Dim satzanti = "hallo " + t.Item2 + "du bist hier nummer " + t.Item1.ToString
+
+        TextBox1.Text = satz
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Private Function suchePerson(v As Integer) As Tuple
+        Dim tt = (id:=1, Name:="Hannes", ort:="Ach", gebdatum:=#08/09/1968#)
+
+
+
+    End Function
+End Class

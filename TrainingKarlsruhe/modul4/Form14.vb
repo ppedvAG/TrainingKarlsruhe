56583b9d848a38eafb12e2f0429e0ee46147a451
Hannes Preishuber <hannesp@ppedv.de>
2018-12-20 08:31:01 +0100

    halber Weihnachtsbaum For If, Substring

--- TrainingKarlsruhe/modul4/Form14.vb
+++ TrainingKarlsruhe/modul4/Form14.vb
@@ -1,17 +1,17 @@
 ﻿Public Class Form14
     Private Sub Form14_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'halber Weihnachtsbaum
-        Dim a(9) As String
-        Dim temp = "***********"
-        For index = 0 To 9
-            If index < 7 Then
-                a(index) = temp.Substring(0, index)
+        Dim baum(9) As String
+        Dim vieleSternchen = "***********"
+        For schleifenzähler = 0 To 9
+            If schleifenzähler < 7 Then
+                baum(schleifenzähler) = vieleSternchen.Substring(0, schleifenzähler)
             Else
-                a(index) = "*"
+                baum(schleifenzähler) = "*"
             End If
 
 
         Next
-        ListBox1.DataSource = a
+        ListBox1.DataSource = baum
     End Sub
 End Class

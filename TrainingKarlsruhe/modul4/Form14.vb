c5c4306782831a0d6759f756fe3c51710535b0b2
Hannes Preishuber <hannesp@ppedv.de>
2018-12-20 08:47:44 +0100

    halber Weihnachtsbaum IF, For Substr

--- TrainingKarlsruhe/modul4/Form14.vb
+++ TrainingKarlsruhe/modul4/Form14.vb
@@ -5,7 +5,7 @@
         Dim vieleSternchen = "***********"
         For schleifenzähler = 0 To 9
             If schleifenzähler < 7 Then
-                baum(schleifenzähler) = vieleSternchen.Substring(0, schleifenzähler)
+                baum(schleifenzähler) = vieleSternchen.Substring(0, schleifenzähler + 1)
             Else
                 baum(schleifenzähler) = "*"
             End If
@@ -15,3 +15,13 @@
         ListBox1.DataSource = baum
     End Sub
 End Class
+'0 *
+'1 **
+'2 ***
+'3
+'4
+
+
+
+'**********
+'Substring(start, wieviel)

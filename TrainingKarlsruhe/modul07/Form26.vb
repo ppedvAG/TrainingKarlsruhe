58562d7dd4318e2dcce59b0586a1f3139a583086
Hannes Preishuber <hannesp@ppedv.de>
2018-12-21 11:02:45 +0100

    shared Class Function

---
+++ TrainingKarlsruhe/modul07/Form26.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form26
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Dim s As String
+        'Dim auto As New kfz3()
+        'Dim d As Date
+        'd = Date.Now
+
+        'Dim lotto = New kifz4
+        'lotto.aktuelleLottoZahl = lotto.neueLottozahl()
+
+        'Label1.Text = lotto.aktuelleLottoZahl.ToString
+
+
+        Label1.Text = kifz4.neueLottozahl().ToString
+    End Sub
+End Class
+
+
---
+++ TrainingKarlsruhe/modul07/kifz4.vb
@@ -0,0 +1,8 @@
+﻿Public Class kifz4
+    Public Shared letzteZufallszahl As Integer
+    Public aktuelleLottoZahl As Integer
+    Public Shared Function neueLottozahl() As Integer
+
+        Return CInt(Rnd() * 49)
+    End Function
+End Class

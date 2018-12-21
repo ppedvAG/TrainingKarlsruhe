019e6d605aa933216e95f4f6b541f59d69b7f57e
Hannes Preishuber <hannesp@ppedv.de>
2018-12-21 09:29:22 +0100

    Klassen Funktion Select Case

---
+++ TrainingKarlsruhe/modul07/Form24.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form24
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim meinauto = New kfz2
+        meinauto.Hersteller = "BMW"
+        meinauto.GeneriereFahrgestellnummer()
+
+        Label1.Text = $"Herstellt{meinauto.Hersteller} mit der Nummer {meinauto.fahrgestellnummer}"
+    End Sub
+End Class
---
+++ TrainingKarlsruhe/modul07/kfz2.vb
@@ -0,0 +1,20 @@
+﻿Public Class kfz2
+    Public fahrgestellnummer As String
+
+    Public farbe As Color
+    Public tuev As Boolean
+    Public Bildpfad As String
+
+    Public Hersteller As String
+    Public Sub GeneriereFahrgestellnummer()
+        Dim s = CInt(Rnd() * 1000).ToString("D7") '(7 stellen führende 0)
+        Select Case Hersteller
+            Case "BMW"
+                fahrgestellnummer = "YK" + s    'drinnen "YK{i:D7}" blöd weil immer wieder, deswegen tostring (D7
+            Case "VW"
+                fahrgestellnummer = "AB" + s
+            Case Else
+                fahrgestellnummer = "ZZ" + s
+        End Select
+    End Sub
+End Class

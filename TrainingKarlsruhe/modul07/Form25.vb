ef1e730917989d82193a17cf715c511e6ba90192
Hannes Preishuber <hannesp@ppedv.de>
2018-12-21 10:06:38 +0100

    Klasse Konstuktor überladung

---
+++ TrainingKarlsruhe/modul07/Form25.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form25
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim meinauto = New kfz3("BMW")
+        Label1.Text = $"Herstellt{meinauto.Hersteller} mit der Nummer {meinauto.fahrgestellnummer}"
+    End Sub
+End Class
---
+++ TrainingKarlsruhe/modul07/kfz3.vb
@@ -0,0 +1,23 @@
+﻿Public Class kfz3
+    Public fahrgestellnummer As String
+
+    Public farbe As Color
+    Public tuev As Boolean
+    Public Bildpfad As String
+
+    Public Hersteller As String
+    Sub New() 'Konstruktor
+
+    End Sub
+    Public Sub New(hersteller As String) 'Konstruktor Überladen
+        Dim s = CInt(Rnd() * 1000).ToString("D7")
+        Select Case hersteller
+            Case "BMW"
+                fahrgestellnummer = "YK" + s
+            Case "VW"
+                fahrgestellnummer = "AB" + s
+            Case Else
+                fahrgestellnummer = "ZZ" + s
+        End Select
+    End Sub
+End Class

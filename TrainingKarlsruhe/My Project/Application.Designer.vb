46c27f9e4769b9f2486244f3f6ab130f0a465678
Hannes Preishuber <hannesp@ppedv.de>
2018-12-21 12:19:58 +0100

    File Access

--- TrainingKarlsruhe/My Project/Application.Designer.vb
+++ TrainingKarlsruhe/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TrainingKarlsruhe.Form27
+            Me.MainForm = Global.TrainingKarlsruhe.Form28
         End Sub
     End Class
 End Namespace
---
+++ TrainingKarlsruhe/modul10/Form28.vb
@@ -0,0 +1,7 @@
+﻿Imports System.IO
+
+Public Class Form28
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        File.AppendAllText("c:\temp\log.txt", Date.Now.ToString + System.Environment.NewLine)
+    End Sub
+End Class

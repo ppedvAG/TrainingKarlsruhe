60813212ca74f1c685c51331f306c8981bb3ce1d
Hannes Preishuber <hannesp@ppedv.de>
2018-12-21 09:54:37 +0100

    Klassen Funktion Select Case  Dictionary

--- TrainingKarlsruhe/modul07/Form24.vb
+++ TrainingKarlsruhe/modul07/Form24.vb
@@ -1,8 +1,13 @@
 ﻿Public Class Form24
+    Dim autoliste As Dictionary(Of String, kfz2) = New Dictionary(Of String, kfz2)
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim meinauto = New kfz2
         meinauto.Hersteller = "BMW"
         meinauto.GeneriereFahrgestellnummer()
+        If Not autoliste.ContainsKey(meinauto.fahrgestellnummer) Then
+            autoliste.Add(meinauto.fahrgestellnummer, meinauto)
+        End If
 
         Label1.Text = $"Herstellt{meinauto.Hersteller} mit der Nummer {meinauto.fahrgestellnummer}"
     End Sub
